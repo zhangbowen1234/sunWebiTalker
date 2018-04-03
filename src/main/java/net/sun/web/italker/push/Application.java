3c4e53b42881a5b3f7f817f5fbdcd558f9d40ae4
bowen <zhangbowen@silversecuritytech.com>
2018-04-03 16:09:38 +0800

    对所有请求添加拦截器进行过滤  添加到application中初始化

--- src/main/java/net/sun/web/italker/push/Application.java
+++ src/main/java/net/sun/web/italker/push/Application.java
@@ -1,5 +1,6 @@
 package net.sun.web.italker.push;
 
+import net.sun.web.italker.push.provider.AuthRequestFilter;
 import net.sun.web.italker.push.provider.GsonProvider;
 import net.sun.web.italker.push.service.AccountService;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -14,6 +15,9 @@ public class Application extends ResourceConfig{
         // 注册逻辑处理的包名
         //packages("net.qiujuer.web.italker.push.net.sun.web.italker.push.service");
         packages(AccountService.class.getPackage().getName());
+
+        // 注册我们的全局请求拦截器
+        register(AuthRequestFilter.class);
 
         // 注册Json解析器
         // register(JacksonJsonProvider.class);
