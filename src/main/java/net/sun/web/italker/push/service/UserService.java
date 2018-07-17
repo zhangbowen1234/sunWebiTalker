c883ce007de5016299983a7e9d72c3688879c1b1
bowen <zhangbowen@silversecuritytech.com>
2018-07-17 16:35:55 +0800

    添加推送工具类和包

--- src/main/java/net/sun/web/italker/push/service/UserService.java
+++ src/main/java/net/sun/web/italker/push/service/UserService.java
@@ -1,11 +1,13 @@
 package net.sun.web.italker.push.service;
 
 import com.google.common.base.Strings;
+import net.sun.web.italker.push.bean.api.base.PushModel;
 import net.sun.web.italker.push.bean.api.base.ResponseModel;
 import net.sun.web.italker.push.bean.api.user.UpdateInfoModel;
 import net.sun.web.italker.push.bean.card.UserCard;
 import net.sun.web.italker.push.bean.db.User;
 import net.sun.web.italker.push.factory.UserFactory;
+import net.sun.web.italker.push.utils.PushDispatcher;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -50,6 +52,7 @@ public class UserService extends BaseService {
     @Produces(MediaType.APPLICATION_JSON)
     public ResponseModel<List<UserCard>> contact() {
         User self = getSelf();
+
         // 拿到我的联系人
         List<User> users = UserFactory.contacts(self);
         // 转换为UserCard
